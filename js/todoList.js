9ea3b40f466c815e9ab3eaec0f46596bd2040365
Hiệu Nguyễn <nguyenthehieua2001@gmail.com>
2023-09-08 17:04:02 +0700

    full code

--- js/todoList.js
+++ js/todoList.js
@@ -5,6 +5,9 @@ export class ToDoList {
     addToDo(todo) {
         this.tdList.push(todo);
     }
+    removeToDo(index) {
+        this.tdList.splice(index, 1);
+    }
     renderToDo() {
         let content = "";
         //Duyệt mảng từ phải qua trái (bắt đầu wor phần tử cuối cùng của mảng)
@@ -14,8 +17,10 @@ export class ToDoList {
           <li>
           <span>${item.textToDo}</span>
            <div class="buttons">
-           <button class="remove"><i class="fa-solid fa-trash-can"></i></button>
-           <button class="completed"><i class="fa-solid fa-circle-check"></i></button>
+           <button class="remove" data-index="${index}" data-status="${item.status}" onclick="deteleToDo(event)"><i class="fa-solid fa-trash-can"></i></button>
+           <button class="complete" data-index="${index}" data-status="${item.status}"onclick="completeToDo(event)">
+           <i class="fa-solid fa-circle-check"></i>
+           </button>
            </div>
           </li>
           `;
